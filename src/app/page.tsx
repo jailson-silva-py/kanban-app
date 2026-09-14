26aa1f1147ca7f153ed386900235bb9cbe9fe7dd
CruelSun0 <jailsonsunshime@gmail.com>
2026-09-14 18:52:54 -0300

    fix: Adjustment of the root page layout and style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,21 @@ import BtnGoToHome from "./_components/BtnLinkGoTo";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-primary font-geist text-text ">
-      <main className="h-full w-full flex items-center justify-center flex-col gap-4 tracking-widest">
-
+    <div className="px-8 py-4 w-screen h-[calc(100vh-100px)] bg-primary font-geist text-text">
+      <main className="h-full w-full flex items-center justify-center flex-col gap-4 tracking-widest font-rethink">
+        <div className="flex flex-col gap-6 items-center justify-center">
         <h1 className="font-marck-script text-7xl tracking-[8px] bg-linear-to-r to-text from-secondary bg-clip-text text-transparent">
           Kanboom
         </h1>
-        <div className="flex flex-col items-center justify-center gap-1 font-light font-rethink ">
-        <h2>Organize suas tarefas com agilidade</h2>
-        <p>Desfrute da suprema organização moderna com confiabilidade e conforto</p>
+        <div className="flex flex-col items-center justify-center gap-4">
+        <h2 className="font-medium text-center text-lg">Organize suas tarefas com agilidade</h2>
+        <p className="font-geist hyphens-auto text-justify font-light text-sm">Desfrute da suprema organização moderna com confiabilidade e conforto.</p>
         </div>
+          <BtnGoToHome  text="Iniciar" href={"/home"} className="group bg-btn cursor-pointer flex items-center justify-center gap-1 w-40 h-10 shadow-default shadow-shadow rounded-sm hover:-translate-y-1">
+          <TbPlaystationTriangle size={24} className="group-hover:size-8 rotate-90 stroke-[1px] duration-500"/>
+          </BtnGoToHome>
 
-
-        <BtnGoToHome  text="Iniciar" href={"/home"} className="group bg-btn cursor-pointer flex items-center justify-center gap-1 w-40 h-10 shadow-default shadow-shadow rounded-sm hover:-translate-y-1">
-        <TbPlaystationTriangle size={24} className="group-hover:size-8 rotate-90 stroke-[1px] duration-500"/>
-        </BtnGoToHome>
-
-
+        </div>
       </main>
     </div>
   );
