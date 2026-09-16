f97d5afc5454c15b1498f3f05ba24f94560d420e
CruelSun0 <jailsonsunshime@gmail.com>
2026-09-16 15:43:40 -0300

    refactor: Blank lines removed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,10 +8,8 @@ const LoginPage = async () => {
   const LogIn = async () => {
     "use server";
     await signIn("google", {redirectTo:"/home"});
-
   }
     return (
-
     <div className="flex justify-center items-center h-[calc(100vh-60px)] w-screen p-4 tracking-wider font-geist">
     <div className="p-8 flex items-center justify-center max-w-93.75 max-h-93.75 w-[90vw] shadow-default shadow-shadow rounded-sm">
         <div className="min-w-25 w-9/10 h-90 flex flex-col items-center justify-center gap-4">
@@ -25,14 +23,7 @@ const LoginPage = async () => {
             <LoginForm isSignIn={false} />
           <span className="text-xs mx-auto">Não possui uma conta? <Link className="underline" href={"/signin"}>Criar conta</Link></span>
         </div>
-
-
-        <h2></h2>
-
-
-
     </div>
-
     </div>
 
     )
